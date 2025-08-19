12416f65da5705aa889187ed8fff2ddf361ce440
AdiletAldazharbekov <aldazharbekov@gmail.com>
2025-08-20 00:46:50 +0600

    fix: use <Link/> from next/link in header

--- app/(gifts-shop)/layout.tsx
+++ app/(gifts-shop)/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import Image from "next/image";
+import Link from "next/link";
 import { Text } from "./components/Text";
 
 export const metadata: Metadata = {
@@ -16,46 +17,40 @@ export default function Layout({
     <div className="mx-auto my-0 overflow-hidden my-clamp">
       <header className="flex justify-center items-center bg-white px-2 py-0 h-16 w-full">
         <div className=" flex justify-between items-center gap-3 w-full max-w-[1276px] h-16 px-0 py-3">
-          <a
-            className="flex items-center gap-2 h-10 px-5 py-2 rounded-xl"
+          <Link
             href="/home"
+            className="flex items-center gap-2 h-10 px-5 py-2 rounded-xl"
           >
             <Image src="/snowflake.svg" alt="Лого" width={24} height={24} />
-            <Text type="ActionSmall">the gifts</Text>
-          </a>
+            <Text type="ActionSmall">the gifts</Text>{" "}
+          </Link>
+
           <nav>
             <ul className="flex justify-center items-center gap-2">
-              <a
+              <Link
                 href="/"
                 className="rounded-xl px-5 py-3 hover:bg-cs-primary-light hover:text-cs-primary"
               >
-                {" "}
-                <Text type="ActionSmall">gifts</Text>
-              </a>
-
-              <a
+                <Text type="ActionSmall">gifts</Text>{" "}
+              </Link>
+              <Link
                 href="/"
                 className="rounded-xl px-5 py-3 hover:bg-cs-primary-light hover:text-cs-primary"
               >
-                {" "}
-                <Text type="ActionSmall">about</Text>
-              </a>
-
-              <a
+                <Text type="ActionSmall">about</Text>{" "}
+              </Link>{" "}
+              <Link
                 href="/"
                 className="rounded-xl px-5 py-3 hover:bg-cs-primary-light hover:text-cs-primary"
               >
-                {" "}
-                <Text type="ActionSmall">best</Text>
-              </a>
-
-              <a
+                <Text type="ActionSmall">best</Text>{" "}
+              </Link>{" "}
+              <Link
                 href="/"
                 className="rounded-xl px-5 py-3 hover:bg-cs-primary-light hover:text-cs-primary"
               >
-                {" "}
-                <Text type="ActionSmall">contacts</Text>
-              </a>
+                <Text type="ActionSmall">contacts</Text>{" "}
+              </Link>
             </ul>
           </nav>
         </div>
