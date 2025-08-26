ff4774a4d412c01db4f60ea78dd6adfb7ebf4bfd
AdiletAldazharbekov <aldazharbekov@gmail.com>
2025-08-26 12:47:26 +0600

    fix: accessibility ul elements, add li. Header menu

--- app/(gifts-shop)/components/HeaderMenu.tsx
+++ app/(gifts-shop)/components/HeaderMenu.tsx
@@ -13,13 +13,14 @@ export default function HeaderMenu() {
     <nav>
       <ul className="flex justify-center items-center gap-2">
         {menuItems.map((item) => (
-          <Link
-            href={item.href}
+          <li
             key={item.label}
-            className="rounded-xl px-5 py-3 text-cs-dark hover:bg-cs-primary-light hover:text-cs-primary transition delay-75 duration-300 ease-in-out"
+            className="rounded-xl text-cs-dark hover:bg-cs-primary-light hover:text-cs-primary transition delay-75 duration-300 ease-in-out"
           >
-            <Text type="ActionSmall">{item.label}</Text>
-          </Link>
+            <Link href={item.href} className="block px-5 py-3">
+              <Text type="ActionSmall">{item.label}</Text>
+            </Link>
+          </li>
         ))}
       </ul>
     </nav>
